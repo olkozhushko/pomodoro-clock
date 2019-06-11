50a40cb9dd6606fca678d96de0b1a86642d2962e
Oleh <olkozhushko@gmail.com>
2019-06-11 14:41:10 +0300

    some chnages and modifications to files

--- src/components/LengthLabel/LengthLabel.jsx
+++ src/components/LengthLabel/LengthLabel.jsx
@@ -3,9 +3,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowUp, faArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 const LengthLabel = ({title, pickedLength, btnId}) => {
+  const titleClass = title === "Break Length" ? 
+    "length-picker__label-title_break" : 
+    "length-picker__label-title_session";
   return (
     <div className="length-picker__label">
-      <h3 className="length-picker__label-title">{title}</h3>
+      <h3 className={`length-picker__label-title ${titleClass}`}>{title}</h3>
       <div className="length-picker__nav-panel">
         <button className={"length-picker__nav-btn nav-btn_up " + btnId}>
           <FontAwesomeIcon
--- src/components/SessionBlock/SessionBlock.jsx
+++ src/components/SessionBlock/SessionBlock.jsx
@@ -2,17 +2,20 @@ import React from "react";
 
 import "./SessionBlock.css";
 
-const SessionBlock = ({minutes, seconds, breakStarted}) => (
+const SessionBlock = ({ minutes, seconds, breakStarted }) => {
+  const titleClass = breakStarted ? "session-block__title_break" : "";
+  return (
     <div className="session-block">
-      <h3 className="session-block__title">{!breakStarted ? "Session" : "Break"}</h3>
+      <h3 className={`session-block__title ${titleClass}`}>
+        {!breakStarted ? "Session" : "Break"}
+      </h3>
       <p className="session-block__timer">
-          <span 
-            className="session-block__timer-data"
-          >
-            {`${minutes}:${seconds}`}
+        <span className="session-block__timer-data">
+          {`${minutes}:${seconds}`}
         </span>
       </p>
     </div>
-)
+  );
+};
 
 export default SessionBlock;
